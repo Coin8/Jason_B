ff44fba9dfc67a01b6e846bd3d9c0d9a9fcbec17
zhangyi <yizhang210244@sohu-inc.com>
2018-06-21 10:21:28 +0800

    修改版本号

---
+++ app/src/main/java/com/coin/b8/ui/activity/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.coin.b8.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.coin.b8.R;
+
+/**
+ * Created by zhangyi on 2018/6/20.
+ */
+public class DetailActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
