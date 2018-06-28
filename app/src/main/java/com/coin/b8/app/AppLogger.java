afcbc49a851cf962d37d8cf5312ac8fc69bb411f
jianlu <jianlu@sohu-inc.com>
2018-06-28 17:46:53 +0800

    update for gradle

--- app/src/main/java/com/coin/b8/app/AppLogger.java
+++ app/src/main/java/com/coin/b8/app/AppLogger.java
@@ -10,17 +10,23 @@ public class AppLogger {
     public static final String APP_TAG = "B8";
 
     // Open(true) or Close(false)
-    private static boolean LOG_SWITCH = false;
+    private static boolean LOG_ONOFF = false;
 
     private AppLogger() {
     }
 
-    public static void changeSwitch(boolean mode){
-        LOG_SWITCH = mode;
+    public static void setOnOff(boolean onOff) {
+        LOG_ONOFF = onOff;
+        d("Set log on-off:" + onOff);
+    }
+
+    public static boolean getLogStatus() {
+        return LOG_ONOFF;
     }
 
     /**
      * Generate title and content for log
+     *
      * @param content log content
      * @return
      */
@@ -34,77 +40,77 @@ public class AppLogger {
     }
 
     public static void d(String content) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.d(APP_TAG, generateLog(content));
     }
 
     public static void d(String content, Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.d(APP_TAG, generateLog(content), tr);
     }
 
     public static void i(String content) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.i(APP_TAG, generateLog(content));
     }
 
     public static void i(String content, Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.i(APP_TAG, generateLog(content), tr);
     }
 
     public static void v(String content) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.v(APP_TAG, generateLog(content));
     }
 
     public static void v(String content, Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.v(APP_TAG, generateLog(content), tr);
     }
 
     public static void w(String content) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.w(APP_TAG, generateLog(content));
     }
 
     public static void w(String content, Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.w(APP_TAG, generateLog(content), tr);
     }
 
     public static void w(Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.w(APP_TAG, tr);
     }
 
     public static void e(String content) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.e(APP_TAG, generateLog(content));
     }
 
     public static void e(String content, Throwable tr) {
-        if (!LOG_SWITCH) {
+        if (!LOG_ONOFF) {
             return;
         }
         Log.e(APP_TAG, generateLog(content), tr);
--- app/src/main/java/com/coin/b8/utils/AppUtil.java
+++ app/src/main/java/com/coin/b8/utils/AppUtil.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import com.coin.b8.app.AppLogger;
 import com.coin.b8.app.B8Application;
 
 /**
@@ -20,7 +21,7 @@ public class AppUtil {
             PackageInfo pi = pm.getPackageInfo(B8Application.getIntstance().getPackageName(), 0);
             versionName = pi.versionName;
         } catch (Exception e) {
-            Log.e("AppUtil",e.getMessage());
+            AppLogger.e(e.getMessage(), e);
         }
         return versionName;
     }
@@ -32,7 +33,7 @@ public class AppUtil {
             PackageInfo pi = pm.getPackageInfo(B8Application.getIntstance().getPackageName(), 0);
             versionCode = pi.versionCode;
         } catch (Exception e) {
-            Log.e("AppUtil", e.getMessage());
+            AppLogger.e(e.getMessage(), e);
         }
         return versionCode;
     }
@@ -42,12 +43,56 @@ public class AppUtil {
             ApplicationInfo appInfo = context.getPackageManager()
                     .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
             if (appInfo != null) {
-                return appInfo.metaData.getString("CHANNEL_NO");
+                String channel = appInfo.metaData.getString("CHANNEL_NO");
+                AppLogger.d("App channel: " + channel);
+                return channel;
             }
         } catch (Exception e) {
-            Log.e("CommonUtils", e.toString());
+            AppLogger.e(e.getMessage(), e);
         }
         return null;
+    }
+
+    /**
+     * Get APP mode. True is debug, false is online.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean getMode(Context context) {
+        try {
+            ApplicationInfo appInfo = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            if (appInfo != null) {
+                boolean mode = appInfo.metaData.getBoolean("MODE");
+                AppLogger.d("App mode:" + mode);
+                return mode;
+            }
+        } catch (Exception e) {
+            AppLogger.e(e.getMessage(), e);
+        }
+        return false;
+    }
+
+    /**
+     * Get log on-off. True is open, false is close.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean getLogOnOff(Context context) {
+        try {
+            ApplicationInfo appInfo = context.getPackageManager()
+                    .getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            if (appInfo != null) {
+                boolean onOff = appInfo.metaData.getBoolean("LOG");
+                AppLogger.d("Log on-off:" + onOff);
+                return onOff;
+            }
+        } catch (Exception e) {
+            AppLogger.e(e.getMessage(), e);
+        }
+        return false;
     }
 
 
